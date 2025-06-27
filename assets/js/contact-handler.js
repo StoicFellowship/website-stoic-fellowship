73fa896a55e601136fe7f78a16811ed745e388e3
Nick <nick@compassfoundry.com>
2025-06-26 22:24:03 -0500

    removed new logo banner

--- assets/js/contact-handler.js
+++ assets/js/contact-handler.js
@@ -1,7 +1,3 @@
-const SUPABASE_URL = 'https://stoic-fellowship.supabase.co'
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZmZpeHBwYWt2d3ZpeWx5d3Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NzM2MDUsImV4cCI6MjA2NjU0OTYwNX0.md7vm--TLrnVr4KP4ghVAQS8b3URpqr_80ePlYF_u9M'
-
 document.getElementById('contactForm').addEventListener('submit', async (e) => {
   e.preventDefault()
 
